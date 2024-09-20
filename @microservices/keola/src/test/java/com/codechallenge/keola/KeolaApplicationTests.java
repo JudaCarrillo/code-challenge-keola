58c3e00a61db6c8f03abda46586d029adc58a04a
Juda Carrillo <judacarrillot@gmail.com>
2024-09-20 02:13:34 -0500

    Initialize Keola microservice with health check and databases.
    Added `KeolaApplication` and `HealthcheckController` to the new Keola microservice. Configured MongoDB and PostgreSQL using Docker Compose. Included necessary application properties for Spring Boot and setup IntelliJ IDEA project files.

---
+++ @microservices/keola/src/test/java/com/codechallenge/keola/KeolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.keola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
