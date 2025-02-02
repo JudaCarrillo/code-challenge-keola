72f30dd53cfe8d660011007797ecbbd9441db520
Juda <judacarrillot@gmail.com>
2025-02-02 12:32:18 -0500

    fix: init sql script

--- src/backoffice/src/main/resources/database/init.sql
+++ src/backoffice/src/main/resources/database/init.sql
@@ -1,15 +1,21 @@
-CREATE TABLE IF NOT EXISTS client (
-    id uuid,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS client
+(
+    id      uuid,
+    name    VARCHAR(255) NOT NULL,
+    email   VARCHAR(255) NOT NULL,
+    phone   VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS product (
-    id uuid,
-    name VARCHAR(255) NOT NULL,
-    slug VARCHAR(255) NOT NULL,
-    stock INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+CREATE TABLE IF NOT EXISTS product
+(
+    id    uuid,
+    name  VARCHAR(255)   NOT NULL,
+    slug  VARCHAR(255)   NOT NULL UNIQUE,
+    stock INT            NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+
+    PRIMARY KEY (id)
 );
